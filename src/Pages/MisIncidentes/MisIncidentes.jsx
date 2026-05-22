c67b974f1c2c9fba983296f3b14cfd1537e398f0
Breitner Ortiz <brayanp13579b@gmail.com>
2026-05-22 18:08:03 -0500

    Areglar Mis Incidentes

--- src/Pages/MisIncidentes/MisIncidentes.jsx
+++ src/Pages/MisIncidentes/MisIncidentes.jsx
@@ -1,64 +1,54 @@
 import { useEffect, useState } from "react";
-
-import {
-  collection,
-  query,
-  where,
-  onSnapshot,
-  orderBy
-} from "firebase/firestore";
-
-import {
-  auth,
-  db
-} from "../../FireBase/config";
-
-import Nav from "../../Components/Nav/Nav";
-
+import { collection, query, where, onSnapshot, orderBy} from "firebase/firestore";
+import { auth, db} from "../../FireBase/config";
 import CardIncidente from "../../Components/CardIncidente/CardIncidente";
-
 import "./MisIncidentes.css";
 
 function MisIncidentes() {
 
-  const [incidentes, setIncidentes] =
-    useState([]);
+  const [incidentes, setIncidentes] =useState([]);
 
   useEffect(() => {
 
-    const usuario = auth.currentUser;
+  const unsubscribeAuth =
+    auth.onAuthStateChanged((usuario) => {
 
-    if (!usuario) return;
+      if (!usuario) return;
 
-    const consulta = query(
-      collection(db, "incidentes"),
+      const consulta = query(
+        collection(db, "incidentes"),
 
-      where(
-        "uid",
-        "==",
-        usuario.uid
-      ),
+        where(
+          "uid",
+          "==",
+          usuario.uid
+        ),
 
-      orderBy("fecha", "desc")
-    );
+      );
 
-    const unsubscribe = onSnapshot(
-      consulta,
-      (snapshot) => {
+      const unsubscribeFirestore =
+        onSnapshot(
+          consulta,
+          (snapshot) => {
 
-        const datos =
-          snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
+            const datos =
+              snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+              }));
 
-        setIncidentes(datos);
-      }
-    );
+            setIncidentes(datos);
+          }
+        );
 
-    return () => unsubscribe();
+      return () => unsubscribeFirestore();
+    });
 
-  }, []);
+  return () => unsubscribeAuth();
+
+}, []);
+ 
+
 
   return (
 
